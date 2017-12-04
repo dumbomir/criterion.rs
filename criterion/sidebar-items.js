dc3211061fac889ac1745881b27035b099552014
Travis CI User <travis@example.org>
2017-12-04 02:52:14 +0000

    Documentation for japaric/criterion.rs@45d9be1

---
+++ criterion/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Bencher","Helper struct to time routines"],["Criterion","The benchmark manager"],["Fun","Representing a function to benchmark together with a name of that function. Used together with `bench_functions` to represent one out of multiple functions under benchmark."]]});
